112770705edb43bc031ce6333400c0d24f574d6d
Soumyacode1-prog <shambhavisoumya10@gmail.com>
2024-12-23 00:58:25 +0530

    reverted

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -66,7 +66,7 @@ const Navbar = () => {
                             
                             {/* Navbar Button */}
                             <div className="theme-btn">
-                                <Link to="/contact">Book appointment Now</Link>
+                                <Link to="/contact">Book appointment </Link>
                             </div>
                         </div>
                     </div>
